9e553984a8d31e522ff68c8127cbe8de74ec2128
Alexandriaotuare <alexandria.otuare@stonybrook.edu>
2023-05-12 13:44:15 +0000

    new changes

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from flask import Flask
+import pandas as pd
 
-app = FastAPI()
+df = pd.read_csv('./data/services2019.csv')
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is an API service for MN ICD code details'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(10)
+    result = top10rows.to_json(orient="records")
+    return result
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
